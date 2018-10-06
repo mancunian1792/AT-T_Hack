1d965e8cc7121357b6a6a727453d1f449a4f2a81
Hitesh <verma.h@husky.neu.edu>
2018-10-06 14:22:46 -0400

    modified play_video.py

--- play_video.py
+++ play_video.py
@@ -1,12 +1,65 @@
 ########### Python 3.6 #############
-import http.client, urllib.request, urllib.parse, urllib.error, base64, requests, json, cv2
+import http.client, urllib.request, urllib.parse, urllib.error, base64, requests, json
 import cv2
 import os
-import keyboard
 import time
-#multiprocessing import Process ???
 
 
+#global paramter
+ans = {}
+ans['answer'] = 'sad'
+
+def face_emotion():
+    emotion_recognition_url = "https://eastus.api.cognitive.microsoft.com/face/v1.0/detect?%s"
+    headers = {
+        # Request headers
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': '2fe46b07420145988dfe17fa53d95c06',
+    }
+
+    params = urllib.parse.urlencode({
+        # Request parameters
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': 'emotion',
+    })
+
+    try:
+        with open("./img_frame161.jpg", "rb") as imageFile:
+            image_data = imageFile.read()
+            # body = bytearray(f)
+        response = requests.post(emotion_recognition_url, headers=headers, data=image_data, params=params)
+        parsed = json.loads(response.text)
+
+        frame_num = 1
+        sad_count = 0
+        happy_count = 0
+        for face in parsed:
+            print(face['faceAttributes']['emotion'])
+            result =  (face['faceAttributes']['emotion']['sadness'] + face['faceAttributes']['emotion']['neutral'] + face['faceAttributes']['emotion']['fear'] +
+            face['faceAttributes']['emotion']['disgust'] + face['faceAttributes']['emotion']['contempt'] +
+            face['faceAttributes']['emotion']['anger'])
+
+            print('result', result)
+
+            if result > face['faceAttributes']['emotion']['happiness']:
+                sad_count+=1
+            else:
+                happy_count+=1
+
+
+        if sad_count > happy_count:
+            ans['answer'] = 'sad'
+            ans['flag'] = True
+            print('\t >> Don\'t be sad ? I am here to help. I \'ll change the song for you.')
+        else:
+            ans['answer'] ='happy'
+            print('\t >> I think you like this artist.')
+            #frame_num+=1
+
+    except Exception as e:
+        print(e)
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 
 
@@ -36,10 +89,12 @@ def playvideo():
         # start processing the image emotions when timer is in multiple of 4
         if timer / 40 == 4 :
             cv2.imwrite(img_name, frame)
+            print("image saved")
+            exit(0)
             processed+=1
 
             # function call to process image
-            # find_emotion(img_name)
+            # face_emotion(img_name)
 
             key = cv2.waitKey(50) # milliseconds
             timer=1
@@ -64,20 +119,9 @@ def playvideo():
     # print('API calls made or number frames processed for emotion detection : {}'.format(processed))
     vc.release()
 
-    #print("\n\n\tAll images deleted. Memory freed. Enjoy Lappy. ;)")
-
-def printit():
-    # run your code
-    print('Time :  ', end='\r')
-    end = time.time()
-    elapsed = int(end - start)
-    print('Time :  {}'.format(elapsed), end='\r')
-
 
 
 
 if __name__ == '__main__':
-    start = time.time()
-    # printit()
-    # find_emotions()
-    playvideo()
+    face_emotion()
+    # playvideo()
